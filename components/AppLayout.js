1389b7c93ec2b0131bf05e5ce1999172714ad6e1
jinwoo <jintakk6531@gmail.com>
2020-12-31 16:34:22 +0900

    12/31 dummy 로그인 폼 구현

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,20 +1,63 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { Col, Input, Menu, Row } from 'antd';
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+import styled from 'styled-components';
+
+// const dummy = {
+//   nickname: '제로초',
+//   Post: [],
+//   Followings: [],
+//   Followers: [],
+//   isLoggedIn: false,
+// };
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <Link href="/">
-        <a>노드버드</a>
-      </Link>
-      <Link href="/profile">
-        <a>프로필</a>
-      </Link>
-      <Link href="/signup">
-        <a>회원가입</a>
-      </Link>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="mail">
+          <SearchInput enterButton />
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.zerocho.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by ZeroCho
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -22,4 +65,5 @@ const AppLayout = ({ children }) => {
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default AppLayout;
