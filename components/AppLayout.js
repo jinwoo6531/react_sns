2b4c8ba598f01ba5aa816da23a2c6606bf692576
jinwoo <jintakk6531@gmail.com>
2021-01-05 18:47:54 +0900

    1/5

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -6,6 +6,21 @@ import LoginForm from './LoginForm';
 import UserProfile from './UserProfile';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+
+
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right:0 !important;
+    margin-left:0 !important;
+  }
+  .ant-col:first-child {
+    padding-left:0 !important;
+  }
+  .ant-col:last-child {
+    padding-right:0 !important;
+  }
+`;
 
 // const dummy = {
 //   nickname: '제로초',
@@ -24,6 +39,7 @@ const AppLayout = ({ children }) => {
 
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
         <Menu.Item key="home">
           <Link href="/">
