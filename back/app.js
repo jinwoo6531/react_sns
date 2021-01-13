f15d62a9561f6f41932a07e7a8e143011035ffcb
jinwoo <jintakk6531@gmail.com>
2021-01-13 22:09:40 +0900

    1/13 시퀄라이즈 적용중

--- back/app.js
+++ back/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express(); //꼭 한번 호출을 해야한다.
+const postRouter = require('./routes/post');
 
-app.get('/', (req,res) => {
-    res.send('hello world');
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.use('/post', postRouter);
+
+app.listen(3065, () => {
+  console.log('실행중');
 });
