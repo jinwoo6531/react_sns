692a5e16e11c97fab0447f0fea122daaaa67df6a
박진우 <jintakk6531@gmail.com>
2021-01-20 12:07:42 +0900

    1/20 시퀄라이즈 진행중

--- back/app.js
+++ back/app.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express(); //꼭 한번 호출을 해야한다.
-const postRouter = require('./routes/post');
+const postRouter = require("./routes/post");
+const db = require("./models");
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+//서버 실행하며 sequelize도 실행해준다.
+db.sequelize
+    .sync()
+    .then(() => {
+        console.log("db 연결 성공");
+    })
+    .catch(console.error);
+
+app.get("/", (req, res) => {
+    res.send("hello world");
 });
 
-app.use('/post', postRouter);
+app.use("/post", postRouter);
 
 app.listen(3065, () => {
-  console.log('실행중');
+    console.log("실행중");
 });
--- back/models/post.js
+++ back/models/post.js
@@ -1,34 +1,34 @@
-const DataTypes = require('sequelize');
+const DataTypes = require("sequelize");
 const { Model } = DataTypes;
 
 module.exports = class Post extends (
-  Model
+    Model
 ) {
-  static init(sequelize) {
-    return super.init(
-      {
-        // id가 기본적으로 들어있다.
-        content: {
-          type: DataTypes.TEXT,
-          allowNull: false,
-        },
-        // RetweetId
-      },
-      {
-        modelName: 'Post',
-        tableName: 'posts',
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci', // 이모티콘 저장
-        sequelize,
-      }
-    );
-  }
-  static associate(db) {
-    db.Post.belongsTo(db.User); // post.addUser, post.getUser, post.setUser
-    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' }); // post.addHashtags
-    db.Post.hasMany(db.Comment); // post.addComments, post.getComments
-    db.Post.hasMany(db.Image); // post.addImages, post.getImages
-    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' }); // post.addLikers, post.removeLikers
-    db.Post.belongsTo(db.Post, { as: 'Retweet' }); // post.addRetweet
-  }
+    static init(sequelize) {
+        return super.init(
+            {
+                // id가 기본적으로 들어있다.
+                content: {
+                    type: DataTypes.TEXT,
+                    allowNull: false,
+                },
+                // RetweetId
+            },
+            {
+                modelName: "Post",
+                tableName: "posts",
+                charset: "utf8mb4",
+                collate: "utf8mb4_general_ci", // 이모티콘 저장
+                sequelize,
+            }
+        );
+    }
+    static associate(db) {
+        db.Post.belongsTo(db.User); // post.addUser, post.getUser, post.setUser
+        db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" }); // post.addHashtags
+        db.Post.hasMany(db.Comment); // post.addComments, post.getComments
+        db.Post.hasMany(db.Image); // post.addImages, post.getImages 하나의 게시글은 이미지를 여러개 가질수있다.
+        db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" }); // post.addLikers, post.removeLikers
+        db.Post.belongsTo(db.Post, { as: "Retweet" }); // post.addRetweet
+    }
 };
